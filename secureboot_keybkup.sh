fabe47ba07283102def31b1af1ef26b15b64a3f5
Luka/Starr <75753483+PerfectMach1ne@users.noreply.github.com>
2024-04-05 23:09:11 +0200

    Update secureboot_keybkup.sh

--- secureboot_keybkup.sh
+++ secureboot_keybkup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Requires efitools
 # Just plop this into the command line, don't run a script
+# https://wiki.archlinux.org/title/Unified_Extensible_Firmware_Interface/Secure_Boot#Assisted_process_with_sbctl
 for var in PK KEK db dbx ; do efi-readvar -v $var -o old_${var}.esl ; done
